69ecba04dc2a37ea0c2f1fa91256e4c827e68a34
vinicio Brejinski <viniciosdb@gmail.com>
2023-05-09 13:22:17 -0300

    first commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Network(object):
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.ramdom.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.ramdom.randn(y, 1) for y in zip(sizes[:-1], sizes[1:])]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
